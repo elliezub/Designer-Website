dd8bcecb236c360d2c13bc5aa1e5f2d3956c08f6
Ellie <112726692+elliezub@users.noreply.github.com>
2023-05-26 10:33:01 -0400

    format prettier

--- src/script.js
+++ src/script.js
@@ -1,130 +1,128 @@
 let navbar = document.querySelector(".navbar");
 
-window.onscroll = function() {
-    scrollFunction()
-}
+window.onscroll = function () {
+  scrollFunction();
+};
 // when you scroll the scroll function is triggered
 
 function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        navbar.classList.add("shrink");
-    } else {
-        navbar.classList.remove("shrink");
-    }
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    navbar.classList.add("shrink");
+  } else {
+    navbar.classList.remove("shrink");
+  }
 }
 
 //this is for the review slideshow area
 // function name changeSlide()
 
-
 //get the div from the DOM
-let slide = document.getElementsByClassName('review-slide'); 
+let slide = document.getElementsByClassName("review-slide");
 //initialize slide number to 0
 let slideNumber = 0;
 
-//loop goes through each slide and hides it 
+//loop goes through each slide and hides it
 function visibleSlide(num) {
-   if (slide.length === 0) { //check if there are any slides on the page
-    return; //if not return immediately 
-   }
-   
-    for (let i = 0; i < slide.length; i++) {
-        slide[i].style.display = "none";
-    }
+  if (slide.length === 0) {
+    //check if there are any slides on the page
+    return; //if not return immediately
+  }
 
-slideNumber = num;
-if (slideNumber >= slide.length) {slideNumber = 0}
-//if the slide number is greater than the total number of slides we will go back to the first slide
-if (slideNumber < 0) {slideNumber = slide.length-1}
-//this one does the opposite
-slide[slideNumber].style.display = "block";
+  for (let i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";
+  }
+
+  slideNumber = num;
+  if (slideNumber >= slide.length) {
+    slideNumber = 0;
+  }
+  //if the slide number is greater than the total number of slides we will go back to the first slide
+  if (slideNumber < 0) {
+    slideNumber = slide.length - 1;
+  }
+  //this one does the opposite
+  slide[slideNumber].style.display = "block";
 }
 
 function changeSlide(num) {
-    visibleSlide(slideNumber += num)
+  visibleSlide((slideNumber += num));
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
-    let slideNumber = 0;
-    visibleSlide(slideNumber);
-})
+  let slideNumber = 0;
+  visibleSlide(slideNumber);
+});
 
+//center the accolades section on nav bar click
 
-//center the accolades section on nav bar click 
-
-const accoladesLink = document.querySelector('.accoladesnavlink');
+const accoladesLink = document.querySelector(".accoladesnavlink");
 
 if (accoladesLink) {
+  accoladesLink.addEventListener("click", (event) => {
+    const accolades = document.getElementById("accolades");
 
-accoladesLink.addEventListener("click", (event) => {
+    if (accolades) {
+      event.preventDefault();
 
-const accolades = document.getElementById("accolades");
+      const container = accolades.getBoundingClientRect();
+      const accoladesPosition = container.top + window.scrollY;
+      const middlePosition = accoladesPosition - window.innerHeight / 4;
 
- if (accolades) {
-    event.preventDefault();
-
-    const container = accolades.getBoundingClientRect();
-    const accoladesPosition = container.top + window.scrollY;
-    const middlePosition = accoladesPosition - (window.innerHeight / 4);
-
-    window.scrollTo({top: middlePosition, behavior: 'smooth'});
- }
-});
+      window.scrollTo({ top: middlePosition, behavior: "smooth" });
+    }
+  });
 }
 
+// MODAL LIGHT BOX LOOK BOOK
 
-// MODAL LIGHT BOX LOOK BOOK 
+let myModal = document.getElementById("modal-slideshow");
+let slides = document.getElementsByClassName("lookbookSlides");
 
-
-let myModal = document.getElementById('modal-slideshow')
-let slides = document.getElementsByClassName('lookbookSlides')
-
-//open slideshow 
+//open slideshow
 function openSlideshow() {
-    myModal.style.display = "block";
+  myModal.style.display = "block";
 }
 
-//close slideshow 
+//close slideshow
 function closeSlideshow() {
-    myModal.style.display = "none";
+  myModal.style.display = "none";
 }
 
 // written first to initialize the slideIndex variable and to call the function on the first load of the page
 let slideIndex = 0;
 showSlides(slideIndex);
 
-// next prev 
+// next prev
 function plusSlides(num) {
-    showSlides(slideIndex += num);
+  showSlides((slideIndex += num));
 }
 
 function showSlides(num) {
-    if (num > slides.length){
-        slideIndex = 0
-}
-   else if (num < 0) {
-        slideIndex = slides.length -1;
-}  else {
-    slideIndex = num
-}
+  if (num > slides.length) {
+    slideIndex = 0;
+  } else if (num < 0) {
+    slideIndex = slides.length - 1;
+  } else {
+    slideIndex = num;
+  }
 
-for (let i = 0; i < slides.length; i++){
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-}
-    slides[slideIndex].style.display = "block";
+  }
+  slides[slideIndex].style.display = "block";
 }
 
 // open slideshow from the image that gets clicked
 function addClickHandler(element, index) {
-    element.onclick = function() {
-        slideIndex = index; 
-        openSlideshow();
-        showSlides(slideIndex);
-    }
+  element.onclick = function () {
+    slideIndex = index;
+    openSlideshow();
+    showSlides(slideIndex);
+  };
 }
 
-let imgElements = document.getElementsByClassName('photo');
+let imgElements = document.getElementsByClassName("photo");
 
-for(let i=0; i<imgElements.length; i++) {
-    addClickHandler(imgElements[i], i);
+for (let i = 0; i < imgElements.length; i++) {
+  addClickHandler(imgElements[i], i);
 }
